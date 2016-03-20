2644b180fadbc182b1d3db9e48ec72a3702d4e8c
Roman Tymchyk <roman@tilt.com>
2016-03-20 15:15:16 -0400

    Revision

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
+var selectors = {
+  navLink: '.nav-container a'
+};
+
+function showTab(tab) {
+  $('.nav-container a[href="' + tab + '"]').addClass('selected');
+  $('.text[data-tab="' + tab + '"]').removeClass('hide');
+
+  $('.nav-container a[href!="' + tab + '"]').removeClass('selected');
+  $('.text[data-tab!="' + tab + '"]').addClass('hide');
+}
+
 $(document).ready(function() {
-  $('a').click(function() {
+  var validHashes = [];
+  $(selectors.navLink).each(function(index, element) {
+    validHashes.push($(element).attr('href'));
+  });
+
+  var hash = window.location.hash;
+  if (hash && validHashes.indexOf(hash) !== -1) {
+    showTab(hash);
+  }
+
+  $(selectors.navLink).click(function() {
     var tab = $(this).attr('href');
-    $(this).addClass('selected');
-    $('a[href!="' + tab + '"]').removeClass('selected');
-    $('.text[data-tab="' + tab + '"]').removeClass('hide');
-    $('.text[data-tab!="' + tab + '"]').addClass('hide');
+    showTab(tab);
   });
 });
